09c8df2d452586ea2debd33fdd097dbd4e5773e0
AlayaElla <515451867@qq.com>
2018-01-30 18:35:28 +0800

    替换资源风格

--- Assets/Script/LevelBegin.cs
+++ Assets/Script/LevelBegin.cs
@@ -9,17 +9,17 @@ public class LevelBegin : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         FindImage(transform);
-        foreach (Transform t in imagelist)
-        {
-            Debug.Log(t.name);
-        }
+        //foreach (Transform t in imagelist)
+        //{
+        //    Debug.Log(t.name);
+        //}
         //imagelist = ShuffleList(imagelist);
         Debug.Log(imagelist.Count);
         float dely = 0;
         foreach (Transform t in imagelist)
         {
             t.localScale = new Vector3(1, 0, 1);
-            LeanTween.scaleY(t.gameObject, 1, 1f).setEase(LeanTweenType.easeOutBounce).setDelay(dely);
+            LeanTween.scaleY(t.gameObject, 1, 1f).setEase(LeanTweenType.easeOutBack).setDelay(dely);
             dely += 0.1f;
         }
     }
@@ -33,9 +33,13 @@ public class LevelBegin : MonoBehaviour {
     {
         foreach (Transform t in transform)
         {
-            if (t.name == "palyerLayer"|| t.name == "teacher")
+            if (t.name == "playerLayer" || t.name.Contains("studentLayer") || t.name.Contains("teacherLayer"))
             {
                 imagelist.Add(t);
+            }
+            else if((t.name.CompareTo("UIlayer")==0))
+            {
+                continue;
             }
             else
             {
